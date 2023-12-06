0e39f084158a83e01a40bd55b14a5402bd2c8ccf
Seydalievnikolay <118976386+Seydalievnikolay@users.noreply.github.com>
2023-12-06 08:50:01 +0700

    Initial commit

---
+++ src/main/java/com/example/skypro/automation/of/sock/warehouse/accounting/Application.java
@@ -0,0 +1,13 @@
+package com.example.skypro.automation.of.sock.warehouse.accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
