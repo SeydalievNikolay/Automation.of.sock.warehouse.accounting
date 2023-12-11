2500140cf3271c0b69c32b9fb53f92020367196f
Seydalievnikolay <118976386+Seydalievnikolay@users.noreply.github.com>
2023-12-11 23:34:31 +0700

    Automation of sock warehouse accounting

--- src/main/java/com/example/skypro/automation/of/sock/warehouse/accounting/controller/Controller.java
+++ src/main/java/com/example/skypro/automation/of/sock/warehouse/accounting/controller/Controller.java
@@ -4,9 +4,12 @@ import com.example.skypro.automation.of.sock.warehouse.accounting.dto.SocksDto;
 import com.example.skypro.automation.of.sock.warehouse.accounting.model.Socks;
 import com.example.skypro.automation.of.sock.warehouse.accounting.service.Operation;
 import com.example.skypro.automation.of.sock.warehouse.accounting.service.SocksService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,26 +20,35 @@ public class Controller {
     public Controller(SocksService socksService) {
         this.socksService = socksService;
     }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The request has been completed",
+                    content = @Content(schema = @Schema(implementation = Socks.class))),
+            @ApiResponse(responseCode = "400", description = "The request parameters are missing or have an incorrect format"),
+            @ApiResponse(responseCode = "500", description = "An error occurred that is independent of the caller")})
     @PostMapping("/income")
     public SocksDto incomeOfSocks(@RequestBody SocksDto socksDto) {
         return socksService.incomeOfSocks(socksDto);
     }
-
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The request has been completed",
+                    content = @Content(schema = @Schema(implementation = Socks.class))),
+            @ApiResponse(responseCode = "400", description = "The request parameters are missing or have an incorrect format"),
+            @ApiResponse(responseCode = "500", description = "An error occurred that is independent of the caller")})
     @PostMapping("/outcome")
     public SocksDto outcomeOfSocks(@RequestBody SocksDto socksDto) {
         return socksService.outcomeOfSocks(socksDto);
     }
-
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The request has been completed",
+                    content = @Content(schema = @Schema(implementation = Socks.class))),
+            @ApiResponse(responseCode = "400", description = "The request parameters are missing or have an incorrect format"),
+            @ApiResponse(responseCode = "500", description = "An error occurred that is independent of the caller")})
     @GetMapping()
     public Optional<Integer> getSocksAmount(@RequestParam String color,
                                             @RequestParam Operation operation,
                                             @RequestParam int cottonPart){
         return socksService.getSocksAmount(color, operation, cottonPart);
 
-    }
-
-    @GetMapping("/all")
-    public List<Socks> getAll(){
-        return socksService.getAll();
     }
 }
--- src/main/java/com/example/skypro/automation/of/sock/warehouse/accounting/service/SocksService.java
+++ src/main/java/com/example/skypro/automation/of/sock/warehouse/accounting/service/SocksService.java
@@ -1,10 +1,6 @@
 package com.example.skypro.automation.of.sock.warehouse.accounting.service;
 
 import com.example.skypro.automation.of.sock.warehouse.accounting.dto.SocksDto;
-import com.example.skypro.automation.of.sock.warehouse.accounting.model.Socks;
-import org.springframework.http.ResponseEntity;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface SocksService {
@@ -14,6 +10,4 @@ public interface SocksService {
     SocksDto outcomeOfSocks(SocksDto socksDto);
 
     Optional<Integer> getSocksAmount(String color, Operation operation, int cottonPart);
-
-    List<Socks> getAll();
 }
